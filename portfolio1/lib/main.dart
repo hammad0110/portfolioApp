dc48453e718e38dd28cd1f1d5fc8656bf96e0acf
hammad khan <hammad2000khan6528@gmail.com>
2022-10-06 04:44:44 +0530

    no sum

--- portfolio1/lib/main.dart
+++ portfolio1/lib/main.dart
@@ -1,9 +1,10 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
     home: HomePage(),
-    theme: ThemeData(primarySwatch: Colors.grey),
+    theme: ThemeData(primarySwatch: const Color.fromARGB(255, 158, 158, 158)),
   ));
 }
 
@@ -16,11 +17,62 @@ class HomePage extends StatelessWidget {
       body: Center(
         child: Column(
           children: <Widget>[
-            CircleAvatar(
-              radius: 60,
-              backgroundImage: AssetImage("assets/hk.jpeg"),
+            Container(
+                width: 500,
+                height: 150,
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                  gradient: LinearGradient(colors: [
+                    Colors.white,
+                    Colors.red,
+                    Colors.white,
+                    Colors.red,
+                    Colors.white,
+                    Colors.red,
+                    Colors.white,
+                    Colors.red,
+                    Colors.white,
+                    Colors.red,
+                    Colors.white,
+                    Colors.red,
+                    Colors.white,
+                    Colors.red,
+                    Colors.white,
+                    Colors.red,
+                    Colors.white,
+                    Colors.red,
+                    Colors.white,
+                    Colors.red,
+                  ]),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey,
+                      blurRadius: 10,
+                    )
+                  ],
+                ),
+                margin: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+                child: Image.asset(
+                  "assets/hk2.png",
+                  alignment: Alignment(-1, -1),
+                  height: 100,
+                  width: 160,
+                )),
+            Container(
+              child: AnimatedTextKit(
+                animatedTexts: [
+                  RotateAnimatedText('EAT',
+                      alignment: Alignment(0.4, -4),
+                      textStyle: TextStyle(fontSize: 40)),
+                  RotateAnimatedText('SLEEP',
+                      alignment: Alignment(0.4, -4),
+                      textStyle: TextStyle(fontSize: 40)),
+                  RotateAnimatedText('CODE',
+                      alignment: Alignment(0.4, -4),
+                      textStyle: TextStyle(fontSize: 40)),
+                ],
+              ),
             ),
-            Text("This is not a test you have been warned"),
           ],
         ),
       ),
